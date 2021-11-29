e505b3388bf6824c0db3ff496867576c346d5ac5
7alias <xc.navi@gmail.com>
2021-11-29 12:54:32 +0200

    Initial commit

---
+++ src/InfiniteCalc.java
@@ -0,0 +1,41 @@
+public class InfiniteCalc {
+
+    double x = 0;
+
+    public double Calc(double operand1, double operand2, char operation) {
+        if (operation == 's') {
+            return 0;
+        } else {
+
+            switch (operation) {
+
+                case '-':
+                    x = operand1 - operand2;
+                    break;
+                case '+':
+                    x = operand1 + operand2;
+                    break;
+                case '*':
+                    x = operand1 * operand2;
+                    break;
+                case '/':
+                    x = operand1 / operand2;
+                    break;
+                case 's':
+                    break;
+                case 'C':
+                    x = 0;
+                    break;
+                default:
+                    break;
+            }
+            return x;
+        }
+    }
+}
+
+
+
+
+
+
